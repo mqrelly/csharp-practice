7ea5f1e145e4b6cfbf5a53022d3ca45f279e3578
Szabadkai Márk <mqrelly@gmail.com>
2017-11-13 14:39:24 +0100

    Add the 99 bottles challenge.

---
+++ CSharpPractice/NinetyNineBottles/Lirics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpPractice.NinetyNineBottles
+{
+    public class Lirics
+    {
+        public string Verse(int bottleCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Verses(int startingVerse, int endingVerse)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Song()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
