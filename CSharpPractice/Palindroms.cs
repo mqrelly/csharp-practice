c79e7eaf0a7181b3c2bcf3ffaf7ce7c4af74e04d
Szabadkai Márk <mqrelly@gmail.com>
2017-11-15 11:43:34 +0100

    Add Longest non-trivial palindrom challenge.

--- CSharpPractice/Palindroms.cs
+++ CSharpPractice/Palindroms.cs
@@ -1,25 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
 
 namespace CSharpPractice
 {
-    /// <summary>
-    /// Implement the IsPalindrom method so that it gives back true when
-    /// the passed in string is the same as reversed and false otherwise.
-    /// </summary>
     [TestFixture]
-    [Easy]
     public class Palindroms
     {
+        /// <summary>
+        /// Implement the IsPalindrom method so that it gives back true when
+        /// the passed in string is the same as reversed and false otherwise.
+        /// </summary>
+        #region Is palindrom problem
+
         private bool IsPalindrom(string s)
         {
             throw new NotImplementedException();
         }
 
+        [Easy]
         [Test]
-        [TestCaseSource(nameof(TestCases))]
+        [TestCaseSource(nameof(PalindromCases))]
         public void IsPalindromTests(string input, bool expectedResult)
         {
             var isPalindrom = IsPalindrom(input);
@@ -27,7 +28,7 @@ namespace CSharpPractice
             Assert.That(isPalindrom, Is.EqualTo(expectedResult));
         }
 
-        public static IEnumerable<TestCaseData> TestCases
+        public static IEnumerable<TestCaseData> PalindromCases
             => new[]
             {
                 new TestCaseData("", true),
@@ -44,5 +45,68 @@ namespace CSharpPractice
                 new TestCaseData("rétipipitér", true),
                 new TestCaseData("with spaces secaps htiw", true)
             };
+
+        #endregion
+
+
+        /// <summary>
+        /// Given an input string find (one of) the longest substrings which is
+        /// a palindrom. But do not include trivial palindroms like the empty
+        /// string, or a one character long string.
+        /// If more longest palindroms exist, it doesn't matter which one the
+        /// method gives back.
+        /// 
+        /// You must first implement the IsPalindrom method in the previous
+        /// challenge, the tests are depending on it.
+        /// </summary>
+        #region Longest palindrom problem
+
+        private bool FindLongestNonTrivialPalindrom(string input, out int startsAt, out int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Medium]
+        [Test]
+        [TestCaseSource(nameof(LongestPalindromCases))]
+        public void LongestNonTrivialPalindromTests(
+            string input,
+            bool shouldContainPalindrom,
+            int expectedLength)
+        {
+            int startsAt, length;
+            var containsPalindrom = FindLongestNonTrivialPalindrom(input, out startsAt, out length);
+
+            if (shouldContainPalindrom)
+            {
+                Assert.That(containsPalindrom, Is.True);
+                Assert.That(length, Is.EqualTo(expectedLength));
+
+                var candidate = input.Substring(startsAt, length);
+                Assert.That(IsPalindrom(candidate), Is.True);
+            }
+            else
+            {
+                Assert.That(containsPalindrom, Is.False);
+            }
+        }
+
+        public static IEnumerable<TestCaseData> LongestPalindromCases
+            => new[]
+            {
+                new TestCaseData("", false, -1),
+                new TestCaseData("a", false, -1),
+                new TestCaseData("aa", true, 2),
+                new TestCaseData("aaa", true, 3),
+                new TestCaseData("abb", true, 2),
+                new TestCaseData("Xaba", true, 3),
+                new TestCaseData("asdffdsA", true, 6),
+                new TestCaseData(".aba asdf aBBa", true, 4),
+                new TestCaseData(".aba asdf aBa", true, 3),
+                new TestCaseData(".aba asdf aBBa.aa", true, 4),
+                new TestCaseData("abcdefABFDEF123456", false, -1),
+            };
+
+        #endregion
     }
 }
